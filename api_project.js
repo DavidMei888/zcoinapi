a41a532f1d25fc119a6bd99db2317dc9ece15294
深海 <meianjun@163.com>
2019-09-23 21:27:00 +0800

    update

---
+++ api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "example-versioning",
+  "version": "0.2.0",
+  "description": "apiDoc versioning example",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-09-23T13:14:57.714Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
